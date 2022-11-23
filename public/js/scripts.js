daffb59afcafb9d907a372079408da7bc66bf497
JP_Janitha <s219302799@deakin.edu.au>
2022-11-23 14:17:20 +1100

    Form

---
+++ public/js/scripts.js
@@ -0,0 +1,49 @@
+const cardList = [
+    {
+        title: "Hacker 2",
+        image: "images/hacker3.png",
+        link: "About hacker 2",
+        desciption: "Demo desciption about hacker 2"
+    },
+    {
+        title: "Hacker 3",
+        image: "images/hacker.jpg",
+        link: "About hacker 3",
+        desciption: "Demo desciption about hacker 3"
+    }
+]
+
+const submitForm = () => {
+    let formData = {};
+    formData.first_name = $('#first_name').val();
+    formData.last_name = $('#last_name').val();
+    formData.password = $('#password').val();
+    formData.email = $('#email').val();
+
+    console.log("Form Data Submitted: ", formData);
+}
+
+const addCards = (items) => {
+    items.forEach(item => {
+        let itemToAppend = '<div class="col s4 center-align">'+
+    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+    '</div><div class="card-content">'+
+    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+    '<div class="card-reveal">'+
+        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+        '<p class="card-text">'+item.desciption+'</p>'+
+      '</div></div></div>';
+      $("#card-section").append(itemToAppend)
+    });
+}
+
+
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('#formSubmit').click(()=>{
+        submitForm();
+    })
+    addCards(cardList);
+    $('.modal').modal();
+  });
